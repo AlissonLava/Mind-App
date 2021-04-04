177cda977eb58069382ebdcd0ed22ce56fc2fab5
AlissonLava <61854869+AlissonLava@users.noreply.github.com>
2021-04-04 10:12:18 -0300

    Criado formulário de cadastro de conteudos

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -4,5 +4,24 @@ class ContentsController < ApplicationController
     def index
         @contents = current_user.contents
     end
+    	
+    def new
+        @content = Content.new
+    end
+
+    def create
+        @content = current_user.contents.build(content_prams)
+
+        if @content.save
+            redirect-to contents_path, notice: 'criado com sucesso'
+        else
+            render :new
+        end
+    end
+
+    private
+    def content_prams
+        params.require(:content).permit(:title, :description)
+
   end
   
