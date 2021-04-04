a645a3351e9dc96471f9945d5019b153d8bd5f5d
AlissonLava <61854869+AlissonLava@users.noreply.github.com>
2021-04-04 14:02:05 -0300

    adicionado campo de pesquisa por tags

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -4,6 +4,10 @@ class ContentsController < ApplicationController
 
     def index
         @contents = current_user.contents
+        tag_names = params[:tags]
+        if tag_names.present?
+            @contents = @contents.joins(:tags).where(tags:{name: tag_names }).distinct
+        end
     end
     	
     def new
