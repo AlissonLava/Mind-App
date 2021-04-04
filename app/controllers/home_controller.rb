7a756bb185714fc330bc754b256ed8fd5951af3e
AlissonLava <61854869+AlissonLava@users.noreply.github.com>
2021-04-04 14:36:43 -0300

    Adicionado nuvem de tags na Home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   before_action :autenticate_user!
   def index
+    @tags_data = current_user.contents.join(:tags).group('tags.name').count
   end
 end
+ 
